557163d1ca2ad27e865adb987a42076515502859
hamzahpane <m.hamzahpane27@gmail.com>
2023-12-23 13:49:08 +0700

    tambahan

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const  log = require('morgan');
+const cors = require('cors');
 const  productV2 = require('./app/product_v2/routes');
 // const ProductRout = require('./app/product/routes');
 const app = express();
 
+app.use(cors());
 app.use(log('dev'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+app.use(express.text());
 app.use('/public' , express.static(path.join(__dirname,'uploads')));
 app.use('/api/v2', productV2);
 
